aadc1cf6809375fd081fb34a70c242663df8816b
03prashantpk <prashantmanwan@gmail.com>
2024-05-05 13:42:30 +0530

    final

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ const App = () => {
   const [userName, setUserName] = useState('');
   const [isLogin, setIsLogin] = useState(false);
   const [savedID, setSavedAppID] = useState('');
+  const [allPagesToThisAccount, setAllPagesToThisAccount] = useState([]);
+  const [pageId, setPageId] = useState('');
+  const [pageAccessToken, setPageAccessToken] = useState('');
+  const [pageLikes, setPageLikes] = useState('');
+  const [getPagesImpression, setPageImpression] = useState([]);
+  const [sinceDate, setSinceDate] = useState('');
+  const [untilDate, setUntilDate] = useState('');
+  const [pgImpression, setPgImpression] = useState(false);
+
+  const [pageAccessCredentials, setPageAccessCredentials] = useState({ userId: '', pageToken: '' });
 
   useEffect(() => {
     (function(d, s, id) {
@@ -29,15 +39,8 @@ const App = () => {
         xfbml: true,
         version: 'v19.0',
       });
-        
-      window.FB.AppEvents.logPageView();
 
-      // // confirm user if they want to login
-      // const ifwantToLogin = window.confirm("Do you want to login with Facebook?" + "\n" + "App ID: " + AppID + "If not, please enter your App ID in the input field below");
-      
-      // if(ifwantToLogin) {
-      //   loginWithFacebook();
-      // }
+      window.FB.AppEvents.logPageView();
 
       window.FB.getLoginStatus(function(response) {
         if(response.status === 'connected') {
@@ -45,7 +48,6 @@ const App = () => {
           setUserID(response.authResponse.userID);
           setUserToken(response.authResponse.accessToken);
 
-          // Fetch user information
           window.FB.api('/me?fields=name,email', function(response) {
             setUserName(response.name);
             setUserEmail(response.email);
@@ -61,11 +63,12 @@ const App = () => {
         setIsLogin(true);
         setUserID(response.authResponse.userID);
         setUserToken(response.authResponse.accessToken);
+        pageAccessCredentials.userId = response.authResponse.userID;
+        pageAccessCredentials.pageToken = response.authResponse.accessToken;
 
-        // Fetch user information
         window.FB.api('/me?fields=name,email', function(response) {
-            setUserName(response.name);
-            setUserEmail(response.email);
+          setUserName(response.name);
+          setUserEmail(response.email);
         });
       } else {
         console.log('User cancelled login or did not fully authorize.');
@@ -74,7 +77,7 @@ const App = () => {
     {
       config_id: '790630596351702'
     }
-  );
+    );
   }
 
   const logout = () => {
@@ -103,56 +106,177 @@ const App = () => {
     window.location.reload();
   }
 
-  const CurrentUrl = window.location.href;
+  const getAllPages = () => {
+    try {
+      if (window.FB) {
+        window.FB.api(
+          `/${userID}/accounts`,
+          'GET',
+          { access_token: userToken },
+          function(response) {
+            setAllPagesToThisAccount(response.data);
+          }
+        );
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handlePageSelect = (e) => {
+    const selectedPageId = e.target.value;
+    const selectedPage = allPagesToThisAccount.find(page => page.id === selectedPageId);
+    if (selectedPage) {
+      setPageId(selectedPage.id);
+      setPageAccessToken(selectedPage.access_token);
+    }
+  }
+
+  const getSelectedPageLikes = () => {
+    try {
+      if (window.FB) {
+        window.FB.api(
+          `/${pageId}?fields=fan_count`,
+          'GET',
+          { access_token: pageAccessToken },
+          function(response) {
+            setPageLikes(response.fan_count);
+          }
+        );
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleSinceChange = (e) => {
+    setSinceDate(e.target.value);
+  }
+
+  const handleUntilChange = (e) => {
+    setUntilDate(e.target.value);
+  }
+
+  const getPageImpression = (since, until) => {
+    try {
+      if (window.FB) {
+        window.FB.api(
+          `/${pageId}/insights/page_impressions`,
+          'GET',
+          { 
+            access_token: pageAccessToken,
+            since: since,
+            until: until
+          },
+          function(response) {
+            console.log(response);
+            setPageImpression(response.data);
+            setPgImpression(true);
+          }
+        );
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <div className="App">
       <div className="image"></div>
       <div className="Content">
-        <h1>Facebook Login</h1>
-        <h4>Fixed: Working Fine Now</h4>
         <div className="clas">
-          <p>I'm using, <span>App type: Consumer - App ID - default</span><br /> Try Using, <span>App type: Business - App ID</span></p>
-        
-          <p>Add your App ID and Add <span>{CurrentUrl}</span>in: <br />Allowed Domains for the JavaScript SDK at  <a href="https://developers.facebook.com/apps/">developers.facebook.com/apps</a> </p>
-      
-            <hr />
+          <div className="cache-controls">
+            <p className='SavedAppID'>Saved App ID: {localStorage.getItem('AppID')} <button className='ClearAppID' onClick={ClearAppID}> Clear </button></p>
+          </div>
 
-          <p className='SavedAppID'>Saved App ID: {localStorage.getItem('AppID')} <button className='ClearAppID' onClick={ClearAppID}> Clear</button></p>
-
-          <p> <span>{localStorage.getItem('AppID') ? "Clear to login with Consumer App ID" : ""}</span></p>
-          
+          <div className="save-data">
           <input type="text" name="" placeholder='Add Your Business App - App ID' id="AppID" />
           <button onClick={saveAppID}>Save</button>
+          </div>
+
+          {isLogin ? 
+          <button className='logWithFb' onClick={logout}>
+            <i class="fab fa-facebook-f"></i>
+            Logout</button>
+          :
+          <button className='logWithFb' onClick={loginWithFacebook}> 
+            <i class="fab fa-facebook-f"></i>
+          Login with Facebook </button>
+        }
           
         </div>
-        <br />
-        {isLogin ? 
-          <button onClick={logout}>Logout</button>
-          :
-          <button onClick={loginWithFacebook}>Login with Facebook  {localStorage.getItem('AppID') ? "Using Business App ID" : "Using Consumer App ID"} </button>
+
+        {
+          userToken && (
+            <div className="PageList">
+            <div className="cta">
+              <p>Get Page Data</p>
+              <button onClick={getAllPages}>Fetch All Pages Related to this Account</button>
+            </div>
+  
+              <div className="pageListData">
+                <p>All the pages Related to this account</p>
+                <select id="pageId" onChange={handlePageSelect} >
+                  <option value="">Select Page</option>
+                  {allPagesToThisAccount.map((page, index) => (
+                    <option key={index} value={page.id}>{page.name}</option>
+                  ))}
+                </select>
+              </div>
+          </div>
+          )
         }
-        <div className="user-card">
-          <h1>{userID ? "Your Data" : ""}</h1>
-          <div className="card" style={{display: userID ? "flex" : "none"}}>
-            <div className="card-item">
-              <h3>UserID:</h3>
-              <p>{userID.slice(0,6)} {userToken ? "*****" : ""}</p>
-            </div>
-            <div className="card-item">
-              <h3>Token:</h3>
-              <p>{userToken.slice(0,10)} {userToken ? "*****" : ""}</p>
-            </div>
-            <div className="card-item">
-              <h3>Email:</h3>
-              <p>{userEmail ? "******" : ""}{userEmail.slice(-10)}</p>
-            </div>
-            <div className="card-item">
-              <h3>Name:</h3>
-              <p>{userName}</p>
-            </div>
+        
+       
+        {
+          pageAccessToken && (
+            <>
+              <p className='selectedpageaccstoken'>Selected Page Id: <span>{pageId}</span> & Access Token:  <span>{pageAccessToken.slice(0, 10) + '...' + pageAccessToken.slice(-10)}</span></p>
+              <button onClick={getSelectedPageLikes}>Get Selected Page Likes</button>
+            </>
+          
+        )
+        }
+
+        {
+          pageLikes && (
+          <div className="SelectedPageLikes">
+            <p>Selected Page Likes: <span>{pageLikes}</span></p>
+          </div>
+          )
+        }
+
+        {
+          pageLikes && (
+            <div className="selectedPageImpression">
+          <div className="ctaimpression">
+            <h2>Get Selected Page Impression</h2>
+            <p>Since:  <input type="date" placeholder='Since' value={sinceDate} onChange={handleSinceChange} /></p>
+            <p>Until: <input type="date" placeholder='Until' value={untilDate} onChange={handleUntilChange} /></p>
+            <button className='ib' onClick={() => getPageImpression(sinceDate, untilDate)}>Get Page Impression</button>
+          </div>
+
+          <div className="impression">
+            {getPagesImpression.map((impression, index) => (
+              <div key={index}>
+              <p className='ttl'>Title: {impression.title}</p>
+              <p className='dis'>Description: {impression.description}</p>
+              <p>
+              Value:
+                <span className="value"> {impression.values.map((value, index) => (
+                  <span className="val" key={index}>{value.value}</span>
+                ))}</span>
+              </p>
+              </div>
+            ))}
           </div>
         </div>
+          )
+        }
+      
       </div>
     </div>
   );
